7a2f4619430acfb13b51a27939082bbbe7b44830
Mark Miller <mark.kilpatrick.miller@gmail.com>
2016-10-14 17:35:47 -0400

    finish adding style to intro page

--- application/components/Landing.js
+++ application/components/Landing.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react'
-import { View, Text, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import React, { Component } from 'react';
+import { Text, TouchableOpacity, Image, View } from 'react-native';
 
-import Icon from 'react-native-vector-icons/Ionicons';
-import NavigationBar from 'react-native-navbar';
 import Colors from '../styles/colors';
+import { landingStyles } from '../styles';
 import { globals } from '../styles';
+
+const BackgroundImage = 'https://s3-us-west-2.amazonaws.com/assembliesapp/welcome%402x.png';
+const Logo = 'https://s3-us-west-2.amazonaws.com/assembliesapp/logo.png';
+const styles = landingStyles;
 
 class Landing extends Component {
 	constructor(props) {
@@ -12,27 +16,40 @@ class Landing extends Component {
 		this.visitDashboard = this.visitDashboard.bind(this)
 	}
 	visitDashboard() {
-		this.props.navigator.push({
-			name: 'Dashboard'
-		});
+		this.props.navigator.push({ name: 'Dashboard' });
 	}
 	render() {
 		let titleConfig = { title: 'Landing', tintColor: 'white' };
 		return (
-			<View style={globals.flexContainer}>
-				<NavigationBar
-					title={titleConfig}
-					tintColor={Colors.brandPrimary}
-				/>
-				<View style={globals.flexCenter}>
-					<Text style={globals.h2}>
-						This is the Landing Page
-					</Text>
-					<TouchableOpacity onPress={this.visitDashboard}>
-						<Text>Go to the Dashboard</Text>
-					</TouchableOpacity>
-				</View>
-			</View>
+			<View style={styles.container}>
+        <View style={styles.container}>
+          <Image
+            style={styles.backgroundImage}
+            source={{ uri: BackgroundImage }}
+          />
+        </View>
+        <View style={globals.flexCenter}>
+          <Image
+            style={styles.logo}
+            source={{ uri: Logo }}
+          />
+          <Text style={[globals.lightText, globals.h2, globals.mb2]}>
+            assemblies
+          </Text>
+          <Text style={[globals.lightText, globals.h4]}>
+            Where Developers Connect
+          </Text>
+        </View>
+        <TouchableOpacity
+          style={globals.button}
+          onPress={this.visitDashboard}
+        >
+          <Icon name='person' size={36} color='white' />
+          <Text style={globals.buttonText}>
+            Go to Dashboard
+          </Text>
+        </TouchableOpacity>
+      </View>
 		)
 	}
 }
